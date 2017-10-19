9168f5b03f29622bce26e16f07beeb70a737d3fe
Woc12138 <32409312+Woc12138@users.noreply.github.com>
2017-10-20 00:37:32 +0800

    Add files via upload

---
+++ 爬新闻标题.py
@@ -0,0 +1,14 @@
+import re
+import requests
+
+
+
+res = requests.get(r"http://news.ncu.edu.cn/")
+res.encoding = "utf-8"
+news_titles1 = re.findall(r'<li.*?><a.*?>(.*)<\/a><span.*?>.*?<\/span><\/li>',str(res.text))
+for news_title in news_titles1:
+    if not "img" in news_title:
+        with open('新闻标题.txt','a+') as f:
+            news_title = news_title + '\n'
+            f.write(news_title)                   
+                            
