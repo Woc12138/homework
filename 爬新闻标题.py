ae8fa202a5023206ea766128d74e255dbe56de6a
Woc12138 <32409312+Woc12138@users.noreply.github.com>
2017-10-21 13:31:14 +0800

    Add files via upload

--- 爬新闻标题.py
+++ 爬新闻标题.py
@@ -7,7 +7,7 @@ res.encoding = "utf-8"
 news_titles1 = re.findall(r'<li.*?><a.*?>(.*)<\/a><span.*?>.*?<\/span><\/li>',str(res.text))
 for news_title in news_titles1:
     if not "img" in news_title:
-        with open('新闻标题.txt','a+') as f:
+        with open('新闻标题.txt','a+',encoding = "utf-8") as f:
             news_title = news_title + '\n'
             f.write(news_title)                   
                             
